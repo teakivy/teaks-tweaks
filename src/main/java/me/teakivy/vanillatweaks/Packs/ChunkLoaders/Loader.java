051d17ba350e2964e8a46dbde03c83539c24d650
TeakIvy <legocjones@gmail.com>
2021-07-08 19:17:37 -0400

    dev-mode enables unfinished Packs

---
+++ src/main/java/me/teakivy/vanillatweaks/Packs/ChunkLoaders/Loader.java
@@ -0,0 +1,46 @@
+package me.teakivy.vanillatweaks.Packs.ChunkLoaders;
+
+import me.teakivy.vanillatweaks.Main;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loader implements Listener {
+
+    Main main = Main.getPlugin(Main.class);
+
+    List<Location> loaded = new ArrayList<>();
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        if (!main.getConfig().getBoolean("config.dev-mode")) return;
+        Item item = event.getItemDrop();
+        if (item.getItemStack().getType() == Material.NETHER_STAR) {
+            if (item.getLocation().add(0, -1, 0).getBlock().getType() == Material.LODESTONE) {
+                item.getLocation().getChunk().setForceLoaded(true);
+                loaded.add(item.getLocation().add(0, -1, 0).getBlock().getLocation());
+                System.out.println("loaded: " + loaded);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBread(BlockBreakEvent event) {
+        if (!main.getConfig().getBoolean("config.dev-mode")) return;
+        if (event.getBlock().getType() == Material.LODESTONE) {
+            if (loaded.contains(event.getBlock().getLocation())) {
+                loaded.remove(event.getBlock().getLocation());
+                event.getBlock().getLocation().getChunk().setForceLoaded(false);
+                System.out.println("loaded: " + loaded);
+            }
+        }
+    }
+
+}
--- src/main/java/me/teakivy/vanillatweaks/Packs/DurabilityPing/DuraPing.java
+++ src/main/java/me/teakivy/vanillatweaks/Packs/DurabilityPing/DuraPing.java
@@ -8,21 +8,22 @@ import org.bukkit.event.player.PlayerItemDamageEvent;
 
 public class DuraPing implements Listener {
 
-//    static Main main = Main.getPlugin(Main.class);
-//
-//    private final CooldownManager pingCooldown = new CooldownManager();
-//
-//    @EventHandler
-//    public void onItemUse(PlayerItemDamageEvent event) {
-//
-//        float durability = (event.getItem().getType().getMaxDurability() - (event.getItem().getDurability() + 1));
-//        float maxDurability = event.getItem().getType().getMaxDurability();
-//        int percentDamaged = (int) ((durability / maxDurability) * 100);
-//        event.getPlayer().sendMessage(percentDamaged + "  " + durability + "  " + maxDurability);
-//
-//        if (main.getConfig().getIntegerList("packs.durability-ping.ping-at-percent").contains(percentDamaged)) {
-//            event.getPlayer().sendMessage("Hey! " + percentDamaged + "%");
-//        }
-//    }
+    static Main main = Main.getPlugin(Main.class);
+
+    private final CooldownManager pingCooldown = new CooldownManager();
+
+    @EventHandler
+    public void onItemUse(PlayerItemDamageEvent event) {
+        if (!main.getConfig().getBoolean("config.dev-mode")) return;
+
+        float durability = (event.getItem().getType().getMaxDurability() - (event.getItem().getDurability() + 1));
+        float maxDurability = event.getItem().getType().getMaxDurability();
+        int percentDamaged = (int) ((durability / maxDurability) * 100);
+        event.getPlayer().sendMessage(percentDamaged + "  " + durability + "  " + maxDurability);
+
+        if (main.getConfig().getIntegerList("packs.durability-ping.ping-at-percent").contains(percentDamaged)) {
+            event.getPlayer().sendMessage("Hey! " + percentDamaged + "%");
+        }
+    }
 
 }
