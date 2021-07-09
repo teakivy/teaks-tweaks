fb90a69f74b1ad3e7177422b413b34618a2aae87
TeakIvy <legocjones@gmail.com>
2021-07-09 10:59:22 -0400

    Added Spawn Pack

---
+++ src/main/java/me/teakivy/vanillatweaks/Commands/SpawnCommand.java
@@ -0,0 +1,69 @@
+package me.teakivy.vanillatweaks.Commands;
+
+import me.teakivy.vanillatweaks.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class SpawnCommand implements CommandExecutor {
+
+    Main main = Main.getPlugin(Main.class);
+    String vt = ChatColor.GRAY + "[" + ChatColor.GOLD.toString() + ChatColor.BOLD + "VT" + ChatColor.GRAY + "] ";
+
+    HashMap<UUID, Long> cooldown = new HashMap<>();
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("spawn")) {
+            if (!main.getConfig().getBoolean("packs.spawn.enabled")) {
+                sender.sendMessage(vt + ChatColor.RED + "This pack is not enabled!");
+                return true;
+            }
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(vt + "This command can only be ran by a Player!");
+                return true;
+            }
+            Player player = (Player) sender;
+
+            if (main.getConfig().getInt("packs.spawn.teleport-cooldown") > 0) {
+                if (cooldown.containsKey(player.getUniqueId())) {
+                    if (cooldown.get(player.getUniqueId()) + (main.getConfig().getInt("packs.spawn.teleport-cooldown") * 1000L) > System.currentTimeMillis()) {
+                        player.sendMessage(vt + ChatColor.RED + "You must wait " + main.getConfig().getInt("packs.spawn.teleport-cooldown") + " seconds between uses of /spawn!");
+                        return true;
+                    }
+                }
+                cooldown.put(player.getUniqueId(), System.currentTimeMillis());
+            }
+
+            if (main.getConfig().getInt("packs.spawn.teleport-delay") == 0) {
+                player.sendMessage(vt + ChatColor.YELLOW + "Teleporting to world spawn...");
+                teleportToSpawn(player);
+            } else if (main.getConfig().getInt("packs.spawn.teleport-delay") > 0) {
+                Location loc = player.getLocation();
+                player.sendMessage(vt + ChatColor.YELLOW + "Teleporting to world spawn...");
+                Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
+                    if (player.getLocation().getX() == loc.getX() && player.getLocation().getY() == loc.getY() && player.getLocation().getZ() == loc.getZ()) {
+                        teleportToSpawn(player);
+                    } else {
+                        player.sendMessage(vt + ChatColor.RED + "You must stand still to teleport!");
+                    }
+                }, main.getConfig().getInt("packs.spawn.teleport-delay") * 20L);
+            }
+
+        }
+        return false;
+    }
+
+    private void teleportToSpawn(Player player) {
+        World world = player.getWorld();
+        player.teleport(world.getSpawnLocation());
+    }
+}
