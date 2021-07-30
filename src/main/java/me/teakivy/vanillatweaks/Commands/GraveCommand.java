42669113dc128e6f7428b473602f3e73a7fe58dd
TeakIvy <legocjones@gmail.com>
2021-07-29 20:58:30 -0400

    Added Graves Command

---
+++ src/main/java/me/teakivy/vanillatweaks/Commands/GraveCommand.java
@@ -0,0 +1,92 @@
+package me.teakivy.vanillatweaks.Commands;
+
+import me.teakivy.vanillatweaks.Main;
+import me.teakivy.vanillatweaks.Packs.Graves.GraveEvents;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+public class GraveCommand implements CommandExecutor {
+
+    Main main = Main.getPlugin(Main.class);
+    String vt = ChatColor.GRAY + "[" + ChatColor.GOLD.toString() + ChatColor.BOLD + "VT" + ChatColor.GRAY + "] ";
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("grave")) {
+            if (!main.getConfig().getBoolean("packs.graves.enabled")) {
+                sender.sendMessage(vt + ChatColor.RED + "This pack is not enabled!");
+                return true;
+            }
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(vt + "This command can only be ran by a Player!");
+                return true;
+            }
+            Player player = (Player) sender;
+
+            if (args.length < 1) {
+                if (main.getConfig().getBoolean("packs.graves.locatable")) {
+                    PersistentDataContainer data = player.getPersistentDataContainer();
+                    if (data.has(new NamespacedKey(main, "vt_graves_last"), PersistentDataType.STRING)) {
+                        player.sendMessage(data.get(new NamespacedKey(main, "vt_graves_last"), PersistentDataType.STRING));
+                    } else {
+                        player.sendMessage(vt + ChatColor.RED + "You don't have a recent grave!");
+                    }
+                } else {
+                    player.sendMessage(vt + ChatColor.RED + "You are not allowed to locate your grave!");
+                }
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("locate")) {
+                if (main.getConfig().getBoolean("packs.graves.locatable")) {
+                    PersistentDataContainer data = player.getPersistentDataContainer();
+                    if (data.has(new NamespacedKey(main, "vt_graves_last"), PersistentDataType.STRING)) {
+                        player.sendMessage(data.get(new NamespacedKey(main, "vt_graves_last"), PersistentDataType.STRING));
+                    } else {
+                        player.sendMessage(vt + ChatColor.RED + "You don't have a recent grave!");
+                    }
+                } else {
+                    player.sendMessage(vt + ChatColor.RED + "You are not allowed to locate your grave!");
+                }
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("key")) {
+                if (!player.isOp()) {
+                    player.sendMessage(vt +ChatColor.RED + "You must be OP to use this command!");
+                    return true;
+                }
+                player.getInventory().addItem(GraveEvents.getGraveKey());
+                player.sendMessage(vt + ChatColor.GREEN + "Enjoy your key!");
+            }
+
+            if (args[0].equalsIgnoreCase("uninstall")) {
+                if (!player.isOp()) {
+                    player.sendMessage(vt +ChatColor.RED + "You must be OP to use this command!");
+                    return true;
+                }
+                for (World world : Bukkit.getWorlds()) {
+                    for (Entity entity : world.getEntities()) {
+                        if (entity.getScoreboardTags().contains("vt_grave")) {
+                            entity.remove();
+                        }
+                    }
+                }
+                player.sendMessage(vt + ChatColor.YELLOW + "Removed all loaded Graves!");
+            }
+
+        }
+
+        return false;
+    }
+}
