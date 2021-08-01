6592cf78a69eb20a1bb826ceffadb949af2fee1d
TeakIvy <legocjones@gmail.com>
2021-07-31 23:31:01 -0400

    Silencer randomly creating errors *should* be fixed

--- src/main/java/me/teakivy/vanillatweaks/Packs/Mobs/SilenceMobs/Silencer.java
+++ src/main/java/me/teakivy/vanillatweaks/Packs/Mobs/SilenceMobs/Silencer.java
@@ -19,21 +19,20 @@ public class Silencer implements Listener {
     public void onSilence(PlayerInteractAtEntityEvent event) {
         if (!main.getConfig().getBoolean("packs.silence-mobs.enabled")) return;
         Entity entity = event.getRightClicked();
-        Bukkit.getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
-            @Override
-            public void run() {
+        try {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
                 if (entity == null) return;
                 Entity entity1 = getEntityByUniqueId(entity.getUniqueId());
                 if (entity1 == null) return;
                 if (entity1.getCustomName() == null) return;
                 if (entity1.getCustomName().equalsIgnoreCase("silence me") || entity1.getCustomName().equalsIgnoreCase("Silence me") || entity1.getCustomName().equalsIgnoreCase("Silence Me") || entity1.getCustomName().equalsIgnoreCase("silence_me")) {
-
                     entity.setSilent(true);
-
                     entity.setCustomName("silenced");
                 }
-            }
-        }, 10L);
+            }, 10L);
+        } catch (NoClassDefFoundError err) {
+            return;
+        }
     }
     public Entity getEntityByUniqueId(UUID uniqueId) {
         for (World world : Bukkit.getWorlds()) {
