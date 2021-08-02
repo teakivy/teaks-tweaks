abd6076aa9e12803ab90d73aeeb122295f861360
TeakIvy <legocjones@gmail.com>
2021-08-02 01:43:13 -0400

    Added force-enable option for Coords Hud

--- src/main/java/me/teakivy/vanillatweaks/Commands/chCommand.java
+++ src/main/java/me/teakivy/vanillatweaks/Commands/chCommand.java
@@ -1,14 +1,11 @@
 package me.teakivy.vanillatweaks.Commands;
 
 import me.teakivy.vanillatweaks.Main;
-import me.teakivy.vanillatweaks.Utils.DataManager.DataManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import javax.xml.crypto.Data;
 
 public class chCommand implements CommandExecutor {
 
@@ -29,9 +26,15 @@ public class chCommand implements CommandExecutor {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 if (args[0].equalsIgnoreCase("toggle")) {
-                    if (!main.chEnabled.contains(player.getUniqueId())) main.chEnabled.add(player.getUniqueId());
-                    else main.chEnabled.remove(player.getUniqueId());
+                    if (main.getConfig().getBoolean("packs.coords-hud.force-enable")) {
+                        player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD.toString() + ChatColor.BOLD + "VT" + ChatColor.GRAY + "] " + ChatColor.RED + "You cannot toggle Coordinates HUD!");
+                        return true;
+                    }
+                    if (!Main.chEnabled.contains(player.getUniqueId())) Main.chEnabled.add(player.getUniqueId());
+                    else Main.chEnabled.remove(player.getUniqueId());
                     player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD.toString() + ChatColor.BOLD + "VT" + ChatColor.GRAY + "] " + ChatColor.GREEN + "Toggled Coords HUD");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Usage: /" + command.getName() + " <option>");
                 }
             } else {
                 sender.sendMessage(ChatColor.RED + "[VT] You must be a player to use this command!");
