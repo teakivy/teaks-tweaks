63bcedfa13cf7aa1d5bdc58192a7940507560d95
TeakIvy <legocjones@gmail.com>
2021-08-08 15:57:14 -0400

    Merged MessageManager & DataManager

--- src/main/java/me/teakivy/vanillatweaks/Messages/MessageHandler.java
+++ src/main/java/me/teakivy/vanillatweaks/Messages/MessageHandler.java
@@ -1,15 +1,15 @@
 package me.teakivy.vanillatweaks.Messages;
 
 import me.teakivy.vanillatweaks.Main;
-import me.teakivy.vanillatweaks.Utils.DataManager.MessageManager;
+import me.teakivy.vanillatweaks.Utils.DataManager.DataManager;
 
 public class MessageHandler {
     static Main main = Main.getPlugin(Main.class);
-    static MessageManager messageManager = main.messages;
+    static DataManager messageManager = main.data;
 
     public static String getMessage(String path) {
-        if (messageManager.getConfig().contains(path)) {
-            return messageManager.getConfig().getString(path);
+        if (messageManager.getConfig().contains("messages." + path)) {
+            return messageManager.getConfig().getString("messages." + path);
         }
         return "";
     }
