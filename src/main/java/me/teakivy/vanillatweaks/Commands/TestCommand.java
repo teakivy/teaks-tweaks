5f5c97d1527fee8da4ad25bf41c975c0707fffd0
TeakIvy <legocjones@gmail.com>
2021-08-29 14:27:51 -0400

    Added Test Command Permissions

--- src/main/java/me/teakivy/vanillatweaks/Commands/TestCommand.java
+++ src/main/java/me/teakivy/vanillatweaks/Commands/TestCommand.java
@@ -2,6 +2,7 @@ package me.teakivy.vanillatweaks.Commands;
 
 import me.teakivy.vanillatweaks.Main;
 import me.teakivy.vanillatweaks.Utils.AbstractCommand;
+import me.teakivy.vanillatweaks.Utils.ErrorType;
 import me.teakivy.vanillatweaks.Utils.MessageHandler;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -22,6 +23,10 @@ public class TestCommand extends AbstractCommand {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
         World world = player.getWorld();
+        if (!sender.hasPermission("vanillatweaks.test.execute")) {
+            sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
+            return true;
+        }
         if (main.getConfig().getBoolean("config.dev-mode")) {
             player.sendMessage(MessageHandler.getCmdMessage("test", "msg"));
         } else {
