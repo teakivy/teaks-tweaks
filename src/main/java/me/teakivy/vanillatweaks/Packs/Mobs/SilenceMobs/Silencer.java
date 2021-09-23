b5d95a8188953b9efe1aabc8c854e125ad767c1e
TeakIvy <legocjones@gmail.com>
2021-09-23 18:08:48 -0400

    Added Minecart silencing (config)

--- src/main/java/me/teakivy/vanillatweaks/Packs/Mobs/SilenceMobs/Silencer.java
+++ src/main/java/me/teakivy/vanillatweaks/Packs/Mobs/SilenceMobs/Silencer.java
@@ -2,26 +2,56 @@ package me.teakivy.vanillatweaks.Packs.Mobs.SilenceMobs;
 
 import me.teakivy.vanillatweaks.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Silencer implements Listener {
 
     static Main main = Main.getPlugin(Main.class);
 
+    ArrayList<EntityType> minecartTypes = new ArrayList<>();
+
     @EventHandler
     public void onSilence(PlayerInteractAtEntityEvent event) {
         if (!main.getConfig().getBoolean("packs.silence-mobs.enabled")) return;
+        if (minecartTypes.isEmpty()) {
+            minecartTypes.add(EntityType.MINECART);
+            minecartTypes.add(EntityType.MINECART_FURNACE);
+            minecartTypes.add(EntityType.MINECART_CHEST);
+            minecartTypes.add(EntityType.MINECART_COMMAND);
+            minecartTypes.add(EntityType.MINECART_TNT);
+            minecartTypes.add(EntityType.MINECART_HOPPER);
+            minecartTypes.add(EntityType.MINECART_MOB_SPAWNER);
+        }
         Entity entity = event.getRightClicked();
+        if (minecartTypes.contains(entity.getType())) {
+            if (!main.getConfig().getBoolean("packs.silence-mobs.allow-minecarts")) return;
+            ItemStack nametag = event.getPlayer().getInventory().getItem(event.getHand());
+            if (nametag.getType() != Material.NAME_TAG) return;
+            if (!nametag.hasItemMeta()) return;
+            if (!Objects.requireNonNull(nametag.getItemMeta()).getDisplayName().equalsIgnoreCase("silence me") && !Objects.requireNonNull(nametag.getItemMeta()).getDisplayName().equalsIgnoreCase("silence_me")) return;
+            entity.setSilent(true);
+            entity.setCustomName("silenced");
+            event.setCancelled(true);
+            if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+                event.getPlayer().getInventory().getItem(event.getHand()).setAmount(nametag.getAmount() - 1);
+            }
+            return;
+        }
         try {
             Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
-                if (entity == null) return;
                 Entity entity1 = getEntityByUniqueId(entity.getUniqueId());
                 if (entity1 == null) return;
                 if (entity1.getCustomName() == null) return;
