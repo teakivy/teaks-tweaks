732aee247ab43de73477be5df20807fcbc54a330
TeakIvy <legocjones@gmail.com>
2021-11-04 20:36:34 -0400

    Added EditSigns unregister

--- src/main/java/me/teakivy/teakstweaks/Packs/TeaksTweaks/EditSigns/EditSigns.java
+++ src/main/java/me/teakivy/teakstweaks/Packs/TeaksTweaks/EditSigns/EditSigns.java
@@ -10,6 +10,7 @@ import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -153,6 +154,10 @@ public class EditSigns implements Listener {
             }
         }
         throw error;
+    }
+
+    public void unregister() {
+        HandlerList.unregisterAll(this);
     }
 
 
