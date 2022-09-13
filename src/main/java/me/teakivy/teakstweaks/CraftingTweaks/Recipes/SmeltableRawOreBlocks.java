c52a88293151c29638cd0909fdd36270020d34b2
TeakIvy <legocjones@gmail.com>
2022-09-12 21:09:11 -0400

    Fixed smelting 2 copper blocks

--- src/main/java/me/teakivy/teakstweaks/CraftingTweaks/Recipes/SmeltableRawOreBlocks.java
+++ src/main/java/me/teakivy/teakstweaks/CraftingTweaks/Recipes/SmeltableRawOreBlocks.java
@@ -20,14 +20,14 @@ public class SmeltableRawOreBlocks extends AbstractRecipe {
         Bukkit.addRecipe(ironRecipe);
         FurnaceRecipe goldRecipe = new FurnaceRecipe(new NamespacedKey(main, "tt_smeltable_raw_gold_block"), new ItemStack(Material.GOLD_BLOCK), Material.RAW_GOLD_BLOCK, 10, 1000);
         Bukkit.addRecipe(goldRecipe);
-        FurnaceRecipe copperRecipe = new FurnaceRecipe(new NamespacedKey(main, "tt_smeltable_raw_copper_block"), new ItemStack(Material.COPPER_BLOCK, 2), Material.RAW_COPPER_BLOCK, 10, 1000);
+        FurnaceRecipe copperRecipe = new FurnaceRecipe(new NamespacedKey(main, "tt_smeltable_raw_copper_block"), new ItemStack(Material.COPPER_BLOCK), Material.RAW_COPPER_BLOCK, 10, 1000);
         Bukkit.addRecipe(copperRecipe);
 
         BlastingRecipe ironBlast = new BlastingRecipe(new NamespacedKey(main, "tt_smeltable_raw_iron_block_blast"), new ItemStack(Material.IRON_BLOCK), Material.RAW_IRON_BLOCK, 10, 600);
         Bukkit.addRecipe(ironBlast);
         BlastingRecipe goldBlast = new BlastingRecipe(new NamespacedKey(main, "tt_smeltable_raw_gold_block_blast"), new ItemStack(Material.GOLD_BLOCK), Material.RAW_GOLD_BLOCK, 10, 600);
         Bukkit.addRecipe(goldBlast);
-        BlastingRecipe copperBlast = new BlastingRecipe(new NamespacedKey(main, "tt_smeltable_raw_copper_block_blast"), new ItemStack(Material.COPPER_BLOCK, 2), Material.RAW_COPPER_BLOCK, 10, 600);
+        BlastingRecipe copperBlast = new BlastingRecipe(new NamespacedKey(main, "tt_smeltable_raw_copper_block_blast"), new ItemStack(Material.COPPER_BLOCK), Material.RAW_COPPER_BLOCK, 10, 600);
         Bukkit.addRecipe(copperBlast);
     }
 }
