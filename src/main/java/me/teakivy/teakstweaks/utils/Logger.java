7dc5675077f59f9c3c3e346e19b7ab4e25c51f8d
TeakIvy <legocjones@gmail.com>
2022-12-11 19:23:14 -0500

    Improved Logger

--- src/main/java/me/teakivy/teakstweaks/utils/Logger.java
+++ src/main/java/me/teakivy/teakstweaks/utils/Logger.java
@@ -7,18 +7,21 @@ public class Logger {
     public static void log(LogLevel level, String message) {
         if (message == null) return;
 
+        String prefix = "";
         switch (level) {
             case ERROR ->
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&c&lERROR&r&8] &f" + message));
+                    prefix = "&8[&c&lERROR&r&8]";
             case WARNING ->
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&6&lWARNING&r&8] &f" + message));
+                    prefix = "&8[&6&lWARNING&r&8]";
             case INFO ->
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&e&lINFO&r&8] &f" + message));
+                    prefix = "&8[&e&lINFO&r&8]";
             case SUCCESS ->
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&a&lSUCCESS&r&8] &f" + message));
+                    prefix = "&8[&a&lSUCCESS&r&8]";
             case OUTLINE ->
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&7" + message));
+                    prefix = "&8[&7&lOUTLINE&r&8]";
         }
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&6&lTeaksTweaks&r&8] " + prefix + " &f" + message));
     }
 
     public enum LogLevel { ERROR, WARNING, INFO, SUCCESS, OUTLINE }
