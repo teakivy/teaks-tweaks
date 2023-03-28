354291d108d4ba125567fb1e007324bfff9218e6
TeakIvy <legocjones@gmail.com>
2023-03-27 21:55:59 -0400

    Attempt to load recipes, if not already

--- src/main/java/me/teakivy/teakstweaks/craftingtweaks/CraftingRegister.java
+++ src/main/java/me/teakivy/teakstweaks/craftingtweaks/CraftingRegister.java
@@ -5,41 +5,44 @@ import me.teakivy.teakstweaks.craftingtweaks.recipes.*;
 public class CraftingRegister {
 
     public static void registerAll() {
-        new BackToBlocks().register();
-        new DoubleSlabs().register();
-        new DropperToDispenser().register();
-        new RottenFleshToLeather().register();
-        new CharcoalToBlackDye().register();
-        new CoalToBlackDye().register();
-        new SandstoneDyeing().register();
-        new UniversalDyeing().register();
-        new StraightToShapeless().register();
-        new BlackstoneCobblestone().register();
-        new PowderToGlass().register();
-        new MoreTrapdoors().register();
-        new MoreBark().register();
-        new MoreStairs().register();
-        new MoreBricks().register();
-        new CraftableGravel().register();
-        new CraftableHorseArmor().register();
-        new CraftableCoral2x2().register();
-        new CraftableCoral3x3().register();
-        new CraftableEnchantedGoldenApples().register();
-        new CraftableNameTags().register();
-        new CraftableBundlesRabbitHide().register();
-        new CraftableBundlesLeather().register();
-        new CraftableBlackstone().register();
-        new CraftableGildedBlackstone().register();
-        new CraftableSporeBlossoms().register();
-        new CraftableSmallDripleaves().register();
-        new CraftableSculkSensors().register();
-        new UnpackableIce().register();
-        new UnpackableNetherWart().register();
-        new UnpackableWool().register();
-        new UnpackableQuartz().register();
-        new LogChests().register();
-        new SmeltableRawOreBlocks().register();
-        new MorePackedMud().register();
+        try {
+            new BackToBlocks().register();
+            new DoubleSlabs().register();
+            new DropperToDispenser().register();
+            new RottenFleshToLeather().register();
+            new CharcoalToBlackDye().register();
+            new CoalToBlackDye().register();
+            new SandstoneDyeing().register();
+            new UniversalDyeing().register();
+            new StraightToShapeless().register();
+            new BlackstoneCobblestone().register();
+            new PowderToGlass().register();
+            new MoreTrapdoors().register();
+            new MoreBark().register();
+            new MoreStairs().register();
+            new MoreBricks().register();
+            new CraftableGravel().register();
+            new CraftableHorseArmor().register();
+            new CraftableCoral2x2().register();
+            new CraftableCoral3x3().register();
+            new CraftableEnchantedGoldenApples().register();
+            new CraftableNameTags().register();
+            new CraftableBundlesRabbitHide().register();
+            new CraftableBundlesLeather().register();
+            new CraftableBlackstone().register();
+            new CraftableGildedBlackstone().register();
+            new CraftableSporeBlossoms().register();
+            new CraftableSmallDripleaves().register();
+            new CraftableSculkSensors().register();
+            new UnpackableIce().register();
+            new UnpackableNetherWart().register();
+            new UnpackableWool().register();
+            new UnpackableQuartz().register();
+            new LogChests().register();
+            new SmeltableRawOreBlocks().register();
+            new MorePackedMud().register();
+        } catch (Exception e) {
+        }
     }
 
 }
