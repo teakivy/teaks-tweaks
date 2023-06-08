d49b0be62fe4a0360e214e5f63ebdc61cbf9cc78
teakivy <legocjones@gmail.com>
2023-06-08 16:47:23 -0400

    Updated Wither Heads to match Vanilla Tweaks

--- src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/WitherHead.java
+++ src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/WitherHead.java
@@ -1,21 +1,44 @@
 package me.teakivy.teakstweaks.packs.mobs.moremobheads.mobs;
 
+import me.teakivy.teakstweaks.packs.mobs.moremobheads.BaseMobHead;
 import me.teakivy.teakstweaks.packs.mobs.moremobheads.Head;
 import me.teakivy.teakstweaks.packs.mobs.moremobheads.MobHeads;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
-public class WitherHead {
-    public static void onDeath(EntityDeathEvent event) {
-        if (!MobHeads.dropChance(event.getEntity().getKiller(), Head.getChance("wither"))) return;
+public class WitherHead extends BaseMobHead {
+
+    public WitherHead() {
+        super(EntityType.WITHER, "wither", Sound.ENTITY_WITHER_SPAWN);
+
+        addHeadTexture("normal", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWRhMTA4MjhmNjNiN2VjZGVmZDc2N2IzMjQ1ZmJkYWExM2MzZWMwYzZiMTM3NzRmMWVlOGQzMDdjMDM0YzM4MyJ9fX0");
+        addHeadTexture("projectile", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjM3YzU4MTRhOTJmOGVjMGY2YWU5OTMzYWJlOTU0MmUxNjUxOTA3NjhlNzYwNDc4NTQzYWViZWVkNDAyN2MyNyJ9fX0");
+        addHeadTexture("blue_projectile", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDM2ODJiMDYyMDNiOWRlNGMyODU0MTA3MWEyNmNkYzM0MGRkMjVkNGMzNzJiNzAyM2VjMmY0MTIwMjFkNjJmNyJ9fX0");
+    }
+
+    @Override
+    @EventHandler
+    public void onDeath(EntityDeathEvent event) {
+        if (event.getEntity().getType() != entity) return;
+        if (!dropHead(event)) return;
+
+
+        event.getDrops().add(getNormalHead());
 
         Random rand = new Random();
         int gen = rand.nextInt(4);
+        switch (gen) {
+            case 0 -> event.getDrops().add(createHead("Wither Projectile Head", this.textures.get("projectile")));
+            case 1 -> event.getDrops().add(createHead("Blue Wither Projectile Head", this.textures.get("blue_projectile")));
+        }
+    }
 
-        if (gen == 0) event.getDrops().add(MobHeads.getHead("Wither", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWRhMTA4MjhmNjNiN2VjZGVmZDc2N2IzMjQ1ZmJkYWExM2MzZWMwYzZiMTM3NzRmMWVlOGQzMDdjMDM0YzM4MyJ9fX0"));
-        if (gen == 1) event.getDrops().add(MobHeads.getHead("Invulnerable Wither", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzE5NmViOGQ1OTljNGZlZjUzYTk3MTc2YjcyZmY4ZmM0MWUzMmE2NmExNTlmZDQ1MTkwYTBkYTE1NDU4N2UxMiJ9fX0"));
-        if (gen == 2) event.getDrops().add(MobHeads.getHead("Armored Wither", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjM3YzU4MTRhOTJmOGVjMGY2YWU5OTMzYWJlOTU0MmUxNjUxOTA3NjhlNzYwNDc4NTQzYWViZWVkNDAyN2MyNyJ9fX0"));
-        if (gen == 3) event.getDrops().add(MobHeads.getHead("Armored Invulnerable Wither", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDM2ODJiMDYyMDNiOWRlNGMyODU0MTA3MWEyNmNkYzM0MGRkMjVkNGMzNzJiNzAyM2VjMmY0MTIwMjFkNjJmNyJ9fX0"));
+    public ItemStack getNormalHead() {
+        return createHead("Wither Head", this.textures.get("normal"));
     }
 }
