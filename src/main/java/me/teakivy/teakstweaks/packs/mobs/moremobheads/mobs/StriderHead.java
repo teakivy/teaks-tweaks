a3d6419136e0fe0332eafc54de7d104fcfb8f1c4
teakivy <legocjones@gmail.com>
2023-06-08 17:54:25 -0400

    Strider Head is no longer random, texture depends on if strider is freezing

--- src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/StriderHead.java
+++ src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/StriderHead.java
@@ -1,19 +1,39 @@
 package me.teakivy.teakstweaks.packs.mobs.moremobheads.mobs;
 
+import me.teakivy.teakstweaks.packs.mobs.moremobheads.BaseMobHead;
 import me.teakivy.teakstweaks.packs.mobs.moremobheads.Head;
 import me.teakivy.teakstweaks.packs.mobs.moremobheads.MobHeads;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Strider;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import java.util.Random;
 
-public class StriderHead {
-    public static void onDeath(EntityDeathEvent event) {
-        if (!MobHeads.dropChance(event.getEntity().getKiller(), Head.getChance("strider"))) return;
+public class StriderHead extends BaseMobHead {
 
-        Random rand = new Random();
-        int gen = rand.nextInt(2);
+    public StriderHead() {
+        super(EntityType.STRIDER, "strider", Sound.ENTITY_STRIDER_AMBIENT);
 
-        if (gen == 0) event.getDrops().add(MobHeads.getHead("Strider", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWM0MGZhZDFjMTFkZTllNjQyMmI0MDU0MjZlOWI5NzkwN2YzNWJjZTM0NWUzNzU4NjA0ZDNlN2JlN2RmODg0In19fQ"));
-        if (gen == 1) event.getDrops().add(MobHeads.getHead("Freezing Strider", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjcxMzA4NWE1NzUyN2U0NTQ1OWMzOGZhYTdiYjkxY2FiYjM4MWRmMzFjZjJiZjc5ZDY3YTA3MTU2YjZjMjMwOSJ9fX0"));
+        addHeadTexture("normal", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWM0MGZhZDFjMTFkZTllNjQyMmI0MDU0MjZlOWI5NzkwN2YzNWJjZTM0NWUzNzU4NjA0ZDNlN2JlN2RmODg0In19fQ");
+        addHeadTexture("freezing", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjcxMzA4NWE1NzUyN2U0NTQ1OWMzOGZhYTdiYjkxY2FiYjM4MWRmMzFjZjJiZjc5ZDY3YTA3MTU2YjZjMjMwOSJ9fX0");
+    }
+
+    @Override
+    public String getTexture(EntityDeathEvent event) {
+        Strider strider = (Strider) event.getEntity();
+
+        if (strider.isShivering()) return this.textures.get("freezing");
+
+        return this.textures.get("normal");
+    }
+
+    @Override
+    public String getName(EntityDeathEvent event) {
+        Strider strider = (Strider) event.getEntity();
+
+        if (strider.isShivering()) return "Freezing Strider";
+
+        return "Strider";
     }
 }
