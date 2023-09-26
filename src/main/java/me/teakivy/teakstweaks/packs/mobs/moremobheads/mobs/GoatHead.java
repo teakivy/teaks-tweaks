19528d8b5943b408df52668b142db84cfb44b80e
TeakIvy <legocjones@gmail.com>
2023-09-26 13:21:36 -0400

    Fixed Goat Head Drops #56

--- src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/GoatHead.java
+++ src/main/java/me/teakivy/teakstweaks/packs/mobs/moremobheads/mobs/GoatHead.java
@@ -12,16 +12,16 @@ public class GoatHead extends BaseMobHead {
         super(EntityType.GOAT, "goat", Sound.ENTITY_GOAT_AMBIENT);
 
         addHeadTexture("goat", "Goat Head", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODc0NzNlMDU1ZGY2ZTdmZDk4NjY0ZTlmZGI2MzY3NWYwODgxMDYzMDVkNzQ0MDI0YTQxYmIzNTg5MThhMTQyYiJ9fX0");
-        addHeadTexture("screaming_goat", "Screaming Goat", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmRhNDg1YWMyMzUxMjQyMDg5MWE1YWUxZThkZTk4OWYwOTFkODQ4ZDE1YTkwNjhkYTQ3MjBkMzE2ZmM0MzMwZiJ9fX0");
+        addHeadTexture("screaming_goat", "Screaming Goat Head", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmRhNDg1YWMyMzUxMjQyMDg5MWE1YWUxZThkZTk4OWYwOTFkODQ4ZDE1YTkwNjhkYTQ3MjBkMzE2ZmM0MzMwZiJ9fX0");
     }
 
     @Override
     public String getTexture(EntityDeathEvent event) {
         Goat goat = (Goat) event.getEntity();
 
-        if (goat.isScreaming()) return "screaming_goat";
+        if (goat.isScreaming()) return this.textures.get("screaming_goat");
 
-        return "goat";
+        return this.textures.get("goat");
     }
 
     @Override
