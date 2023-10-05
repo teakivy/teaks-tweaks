dcb415dae88490c7b4853d9fc534fb5698a8c4f7
TeakIvy <legocjones@gmail.com>
2023-10-04 21:59:12 -0400

    Updated workstationhighlight command to translatable

--- src/main/java/me/teakivy/teakstweaks/utils/MessageHandler.java
+++ src/main/java/me/teakivy/teakstweaks/utils/MessageHandler.java
@@ -37,43 +37,4 @@ public class MessageHandler {
         return str;
     }
 
-    public static String getCmdName(String cmd) {
-        if (messageManager.getConfig().contains("commands." + cmd + ".name")) {
-            return messageManager.getConfig().getString("commands." + cmd + ".name");
-        }
-        return "";
-    }
-
-    public static String getCmdUsage(String cmd) {
-        if (messageManager.getConfig().contains("commands." + cmd + ".usage")) {
-            return messageManager.getConfig().getString("commands." + cmd + ".usage");
-        }
-        return "";
-    }
-
-    public static String getCmdDescription(String cmd) {
-        if (messageManager.getConfig().contains("commands." + cmd + ".description")) {
-            return messageManager.getConfig().getString("commands." + cmd + ".description");
-        }
-        return "";
-    }
-
-    public static List<String> getCmdAliases(String cmd) {
-        if (messageManager.getConfig().contains("commands." + cmd + ".name")) {
-            return messageManager.getConfig().getStringList("commands." + cmd + ".aliases");
-        }
-        return null;
-    }
-
-    public static String getCmdMessage(String cmd, String path) {
-        if (messageManager.getConfig().contains("commands." + cmd + ".messages." + path)) {
-            return ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(
-                    messageManager.getConfig().getString("commands." + cmd + ".messages." + path)
-                            .replace("%prefix%", "")
-                            .replace("%bland_prefix%", "")
-            ));
-        }
-        return "";
-    }
-
 }
