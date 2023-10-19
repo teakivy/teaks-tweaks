0427cfaf7b74be8b5777290e475aae1d9f4b38c2
TeakIvy <legocjones@gmail.com>
2023-10-19 00:18:19 -0400

    Updated Logger to include #info #error #warning #success & #outline

--- src/main/java/me/teakivy/teakstweaks/utils/Logger.java
+++ src/main/java/me/teakivy/teakstweaks/utils/Logger.java
@@ -4,6 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class Logger {
+    /**
+     * Logs a message to the console
+     * @param level The log level
+     * @param message The message
+     * @param toAdmins If the message should be sent to admins in-game
+     */
     public static void log(LogLevel level, String message, boolean toAdmins) {
         if (message == null) return;
 
@@ -28,10 +34,58 @@ public class Logger {
         Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("teakstweaks.manage")).forEach(player -> player.sendMessage(ChatColor.YELLOW + message));
     }
 
+    /**
+     * Logs a message to the console
+     * @param level The log level
+     * @param message The message
+     */
     public static void log(LogLevel level, String message) {
         log(level, message, false);
     }
 
+    /**
+     * Logs an error message to the console
+     * @param message The message
+     */
+    public static void error(String message) {
+        log(LogLevel.ERROR, message);
+    }
+
+    /**
+     * Logs a warning message to the console
+     * @param message The message
+     */
+    public static void warning(String message) {
+        log(LogLevel.WARNING, message);
+    }
+
+    /**
+     * Logs an info message to the console
+     * @param message The message
+     */
+    public static void info(String message) {
+        log(LogLevel.INFO, message);
+    }
+
+    /**
+     * Logs a success message to the console
+     * @param message The message
+     */
+    public static void success(String message) {
+        log(LogLevel.SUCCESS, message);
+    }
+
+    /**
+     * Logs an outline message to the console
+     * @param message The message
+     */
+    public static void outline(String message) {
+        log(LogLevel.OUTLINE, message);
+    }
+
+    /**
+     * Logger levels
+     */
     public enum LogLevel { ERROR, WARNING, INFO, SUCCESS, OUTLINE }
 
 }
