8bc17af2a3c50518a991485189cc500d8afc958c
TeakIvy <legocjones@gmail.com>
2023-10-29 04:08:04 -0400

    Rewrote /spawningspheres to new system

--- src/main/java/me/teakivy/teakstweaks/commands/SpawningSpheresCommand.java
+++ src/main/java/me/teakivy/teakstweaks/commands/SpawningSpheresCommand.java
@@ -1,120 +1,76 @@
 package me.teakivy.teakstweaks.commands;
 
-import me.teakivy.teakstweaks.TeaksTweaks;
 import me.teakivy.teakstweaks.packs.utilities.spawningspheres.SphereType;
 import me.teakivy.teakstweaks.packs.utilities.spawningspheres.SpheresPack;
-import me.teakivy.teakstweaks.utils.ErrorType;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SpawningSpheresCommand extends AbstractCommand {
 
     public SpawningSpheresCommand() {
-        super("spawning-spheres", "spawningspheres", "/spawningspheres <create|remove|teleport> <red|blue|green>", "Spawn a sphere to help with mob spawning", List.of("ss", "sphere"));
+        super("spawning-spheres", "spawningspheres", "/spawningspheres <create|remove|teleport> <red|blue|green>", List.of("ss", "sphere"), CommandType.PLAYER_ONLY);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!sender.hasPermission(permission)) {
-            sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
-            return true;
-        }
-
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ErrorType.NOT_PLAYER.m());
-            return true;
-        }
-        Player player = (Player) sender;
-
+    public void playerCommand(Player player, String[] args) {
         if (args.length < 2) {
-            player.sendMessage(getUsage());
-            return true;
+            sendUsage(player);
+            return;
         }
 
         SphereType type = SphereType.getSphereType(args[1]);
         if (type == null) {
-            player.sendMessage(getString("error.invalid_color"));
-            return true;
+            player.sendMessage(getError("invalid_color"));
+            return;
         }
 
         if (args[0].equalsIgnoreCase("create")) {
-            if (!sender.hasPermission(permission+".create")) {
-                sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
-                return true;
-            }
+            if (!checkPermission(player, "create")) return;
 
             boolean success = SpheresPack.summonSphere(type, player.getLocation());
             if (!success) {
                 player.sendMessage(getString("error.in_use").replace("%color%", type.getName()));
-                return true;
+                return;
             }
 
             player.sendMessage(getString("summoned").replace("%color%", type.getName()));
-            return true;
+            return;
         }
 
         if (args[0].equalsIgnoreCase("remove")) {
-            if (!sender.hasPermission(permission+".remove")) {
-                sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
-                return true;
-            }
+            if (!checkPermission(player, "remove")) return;
 
             boolean success = SpheresPack.removeSphere(type, player);
             if (!success) {
                 player.sendMessage(getString("error.not_in_use").replace("%color%", type.getName()));
-                return true;
+                return;
             }
 
             player.sendMessage(getString("removing").replace("%color%", type.getName()));
-            return true;
+            return;
         }
 
-        if (args[0].equalsIgnoreCase("teleport") || args[0].equalsIgnoreCase("tp")) {
-            if (!sender.hasPermission(permission+".teleport")) {
-                sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
-                return true;
-            }
+        if (args[0].equalsIgnoreCase("teleport")) {
+            if (!checkPermission(player, "teleport")) return;
 
             boolean success = SpheresPack.teleport(type, player);
             if (!success) {
                 player.sendMessage(getString("error.not_in_use").replace("%color%", type.getName()));
-                return true;
+                return;
             }
 
             player.sendMessage(getString("teleporting").replace("%color%", type.getName()));
-            return true;
+            return;
         }
 
-        player.sendMessage(getUsage());
-        return true;
+        sendUsage(player);
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        List<String> tabComplete = new ArrayList<>();
-
-        if (args.length == 1) {
-            tabComplete.add("create");
-            tabComplete.add("remove");
-            tabComplete.add("teleport");
-        }
-
-        if (args.length == 2) {
-            tabComplete.add("red");
-            tabComplete.add("blue");
-            tabComplete.add("green");
-        }
-
-        List<String> result = new ArrayList<>();
-        for (String a : tabComplete) {
-            if (a.toLowerCase().startsWith(args[args.length - 1].toLowerCase()))
-                result.add(a);
-        }
-
-        return result;
+    public List<String> tabComplete(String[] args) {
+        if (args.length == 1) return List.of("create", "remove", "teleport");
+        if (args.length == 2) return List.of("red", "blue", "green");
+        return null;
     }
 }
