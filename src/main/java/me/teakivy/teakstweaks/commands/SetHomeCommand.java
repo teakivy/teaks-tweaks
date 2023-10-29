8d338f603fcd0fe634a221eb36d7b41ac19d1239
TeakIvy <legocjones@gmail.com>
2023-10-29 03:46:46 -0400

    Rewrote /sethome to new system

--- src/main/java/me/teakivy/teakstweaks/commands/SetHomeCommand.java
+++ src/main/java/me/teakivy/teakstweaks/commands/SetHomeCommand.java
@@ -1,11 +1,7 @@
 package me.teakivy.teakstweaks.commands;
 
-import me.teakivy.teakstweaks.TeaksTweaks;
 import me.teakivy.teakstweaks.packs.teleportation.homes.Home;
 import me.teakivy.teakstweaks.packs.teleportation.homes.HomesPack;
-import me.teakivy.teakstweaks.utils.ErrorType;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -13,42 +9,36 @@ import java.util.List;
 public class SetHomeCommand extends AbstractCommand {
 
     public SetHomeCommand() {
-        super("homes", "sethome", "/sethome", "Set a home to teleport to at a later date");
+        super("homes", "sethome", "/sethome [name]", CommandType.PLAYER_ONLY);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player player = (Player) sender;
+    public void playerCommand(Player player, String[] args) {
         List<Home> homes = HomesPack.getHomes(player);
 
-        if (!sender.hasPermission(permission)) {
-            sender.sendMessage(ErrorType.MISSING_COMMAND_PERMISSION.m());
-            return true;
-        }
-
         if (args.length < 1 && HomesPack.getHome(player, "home") != null) {
-            player.sendMessage(get("homes.error.missing_home_name"));
-            return true;
+            player.sendMessage(get("home.error.missing_home_name"));
+            return;
         }
 
         String name = args.length < 1 ? "home" : args[0].toLowerCase();
 
         if (HomesPack.getHome(player, name) != null) {
-            player.sendMessage(get("homes.error.home_already_exists").replace("%name%", name));
-            return true;
+            player.sendMessage(get("home.error.home_already_exists").replace("%name%", name));
+            return;
         }
 
-        int maxHomes = TeaksTweaks.getInstance().getConfig().getInt("packs.homes.max-homes");
+        int maxHomes = getConfig().getInt("packs.homes.max-homes");
         if (maxHomes > 0 && homes.size() >= maxHomes) {
-            player.sendMessage(get("homes.error.max_homes").replace("%max_homes%", maxHomes + ""));
-            return true;
+            player.sendMessage(get("home.error.max_homes").replace("%max_homes%", maxHomes + ""));
+            return;
         }
 
-        if (HomesPack.setHome(player, name, player.getLocation())) {
-            player.sendMessage(getString("homes.set_home").replace("%name%", name));
-        } else {
-            player.sendMessage(get("homes.error.cant_set_home"));
+        if (!HomesPack.setHome(player, name, player.getLocation())) {
+            player.sendMessage(get("home.error.cant_set_home"));
+            return;
         }
-        return true;
+
+        player.sendMessage(getString("home.set_home").replace("%name%", name));
     }
 }
