4786e5c9f71351840611f1fd36fe0bca6f41c91c
TeakIvy <legocjones@gmail.com>
2024-09-05 22:50:27 -0400

    Removed unused ByteArray Item Serializer

--- src/main/java/me/teakivy/teakstweaks/utils/ItemSerializer.java
+++ src/main/java/me/teakivy/teakstweaks/utils/ItemSerializer.java
@@ -20,43 +20,6 @@ import static net.minecraft.nbt.NbtUtils.getDataVersion;
 
 public class ItemSerializer {
 
-//    public static byte[] toByteArray(ItemStack item){
-//        try {
-//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-//
-//            // Write the size of the inventory
-//            dataOutput.writeInt(1);
-//
-//            // Save every element in the list
-//
-//            dataOutput.writeObject(item);
-//
-//            // Serialize that array
-//            dataOutput.close();
-//            return outputStream.toByteArray();
-//        } catch (Exception e) {
-//            throw new IllegalStateException("Unable to save item stacks.", e);
-//        }
-//    }
-//
-//    public static ItemStack fromByteArray(byte[] bytes){
-//        try {
-//            ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-//            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-//
-//            ItemStack item = (ItemStack) dataInput.readObject();
-//
-//            dataInput.close();
-//            System.out.println(item);
-//            return item;
-//        } catch (ClassNotFoundException | IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return null;
-//    }
-
     public static byte[] toByteArray(ItemStack item) {
         Preconditions.checkNotNull(item, "null cannot be serialized");
         Preconditions.checkArgument(item.getType() != Material.AIR, "air cannot be serialized");
