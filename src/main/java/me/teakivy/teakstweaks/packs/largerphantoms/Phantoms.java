3f4fc4d08dddc94fe8313e183e0fdcec139fccad
TeakIvy <legocjones@gmail.com>
2024-09-06 16:12:02 -0400

    Lowered requirements to spawn the mega phantom from 33.3 hours to 24 hours

--- src/main/java/me/teakivy/teakstweaks/packs/largerphantoms/Phantoms.java
+++ src/main/java/me/teakivy/teakstweaks/packs/largerphantoms/Phantoms.java
@@ -38,16 +38,16 @@ public class Phantoms extends BasePack {
         }
         if (player == null || distance > 1000) return;
         int lastSleepTicks = player.getStatistic(Statistic.TIME_SINCE_REST);
-        if (lastSleepTicks < 144000) return;
-        if (lastSleepTicks < 216000) {
+        if (lastSleepTicks < 144000) return; // 2 Hours
+        if (lastSleepTicks < 216000) { // 3 Hours
             setPhantomAttributes(phantom, 3, 25, 1, 20, 15);
             return;
         }
-        if (lastSleepTicks < 288000) {
+        if (lastSleepTicks < 288000) { // 4 Hours
             setPhantomAttributes(phantom, 5, 30, 1.3, 24, 17);
             return;
         }
-        if (lastSleepTicks < 2400000) {
+        if (lastSleepTicks < 1728000) { // 24 Hours
             setPhantomAttributes(phantom, 7, 35, 1.6, 28, 20);
             return;
         }
