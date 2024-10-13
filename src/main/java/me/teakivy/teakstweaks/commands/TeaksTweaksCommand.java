561a8ed6565660a971feba2d5ad2797260a21018
TeakIvy <legocjones@gmail.com>
2024-10-13 01:02:42 -0400

    Added /teakstweaks paste

--- src/main/java/me/teakivy/teakstweaks/commands/TeaksTweaksCommand.java
+++ src/main/java/me/teakivy/teakstweaks/commands/TeaksTweaksCommand.java
@@ -6,16 +6,23 @@ import me.teakivy.teakstweaks.packs.homes.HomesPack;
 import me.teakivy.teakstweaks.utils.Wiki;
 import me.teakivy.teakstweaks.utils.command.*;
 import me.teakivy.teakstweaks.utils.config.Config;
+import me.teakivy.teakstweaks.utils.lang.Translatable;
+import me.teakivy.teakstweaks.utils.log.PasteBookUploader;
+import me.teakivy.teakstweaks.utils.log.PasteManager;
 import me.teakivy.teakstweaks.utils.permission.Permission;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TeaksTweaksCommand extends AbstractCommand {
+    private static final int PASTE_COOLDOWN = 30 * 60 * 60 * 1000;
+    private static long lastPaste = 0;
+
 
     public TeaksTweaksCommand() {
-        super(CommandType.ALL, null, "teakstweaks", Permission.COMMAND_TEAKSTWEAKS, Arrays.asList("tweaks", "tt"), "teakstweakscommand", Arg.optional("info", "version", "support", "update", "wiki"), Arg.optional("packs", "craftingtweaks", "commands"));
+        super(CommandType.ALL, null, "teakstweaks", Permission.COMMAND_TEAKSTWEAKS, Arrays.asList("tweaks", "tt"), "teakstweakscommand", Arg.optional("info", "version", "support", "update", "wiki", "paste"), Arg.optional("packs", "craftingtweaks", "commands", "true", "false"));
     }
 
     @Override
@@ -39,6 +46,9 @@ public class TeaksTweaksCommand extends AbstractCommand {
                 return;
             case "wiki":
                 handleWiki(event);
+                return;
+            case "paste":
+                handlePaste(event);
                 return;
             default:
                 sendUsage();
@@ -82,10 +92,41 @@ public class TeaksTweaksCommand extends AbstractCommand {
         }
     }
 
+    public void handlePaste(CommandEvent event) {
+        System.out.println("Handling paste");
+        if (!checkPermission(Permission.COMMAND_TEAKSTWEAKS_PASTE)) return;
+        System.out.println("No permission");
+        if (System.currentTimeMillis() - lastPaste < PASTE_COOLDOWN) {
+            sendMessage("paste.error.cooldown");
+            return;
+        }
+        System.out.println("No cooldown");
+
+        sendMessage("paste.uploading");
+
+        String playerName = event.getSender().getName();
+        boolean logs = true;
+
+        if (event.isArg(1)) {
+            if (event.getArg(1).equalsIgnoreCase("false")) logs = false;
+        }
+
+        String paste = PasteManager.getPasteContent(playerName, logs);
+        try {
+            String url = PasteBookUploader.uploadText(paste, "Teak's Tweaks Support: " + playerName);
+            sendMessage("paste.success", insert("url", url));
+            lastPaste = System.currentTimeMillis();
+        } catch (IOException e) {
+            sendMessage("paste.error");
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public List<String> tabComplete(TabCompleteEvent event) {
-        if (event.isArg(0)) return Arrays.asList("info", "version", "support", "update", "wiki");
+        if (event.isArg(0)) return Arrays.asList("info", "version", "support", "update", "wiki", "paste");
         if (event.isArg(1) && event.isArg(0, "wiki")) return Arrays.asList("packs", "craftingtweaks", "commands");
+        if (event.isArg(1) && event.isArg(0, "paste")) return Arrays.asList("true", "false");
 
         return null;
     }
--- src/main/java/me/teakivy/teakstweaks/commands/TestCommand.java
+++ src/main/java/me/teakivy/teakstweaks/commands/TestCommand.java
@@ -3,27 +3,16 @@ package me.teakivy.teakstweaks.commands;
 import me.teakivy.teakstweaks.utils.command.AbstractCommand;
 import me.teakivy.teakstweaks.utils.command.CommandType;
 import me.teakivy.teakstweaks.utils.command.PlayerCommandEvent;
-import me.teakivy.teakstweaks.utils.log.PasteBookUploader;
 import me.teakivy.teakstweaks.utils.permission.Permission;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
-
-import java.io.IOException;
 
 public class TestCommand extends AbstractCommand {
 
     public TestCommand() {
-        super(CommandType.PLAYER_ONLY, "test", Permission.COMMAND_TEAKSTWEAKS);
+        super(CommandType.PLAYER_ONLY, "test", Permission.TEST);
     }
 
     @Override
     public void playerCommand(PlayerCommandEvent event) {
         sendMessage("test", insert("name", event.getPlayer().getName()));
-
-        try {
-            String reportUrl = PasteBookUploader.uploadText("Hello, World!", "Teak's Tweaks Test");
-            event.getPlayer().sendMessage(reportUrl);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
