18130665348c107a7ebccf4bb2adee9be03d26c0
TeakIvy <legocjones@gmail.com>
2024-10-21 18:10:59 -0400

    Fix Cave Spider Mob Heads having the wrong key

--- src/main/java/me/teakivy/teakstweaks/packs/moremobheads/mobs/CaveSpiderHead.java
+++ src/main/java/me/teakivy/teakstweaks/packs/moremobheads/mobs/CaveSpiderHead.java
@@ -7,6 +7,6 @@ import org.bukkit.entity.EntityType;
 public class CaveSpiderHead extends BaseMobHead {
 
     public CaveSpiderHead() {
-        super(EntityType.CAVE_SPIDER, "cave-spider", Sound.ENTITY_SPIDER_AMBIENT, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWY4NDNiYjdhOTg0ZTczNGFiNzZhODhjOWExYTBmNWE0MGJmNzk1MjQ4MDlhODUxMWJmMzJkMDU3NTI2ZjdmMyJ9fX0");
+        super(EntityType.CAVE_SPIDER, "cave_spider", Sound.ENTITY_SPIDER_AMBIENT, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWY4NDNiYjdhOTg0ZTczNGFiNzZhODhjOWExYTBmNWE0MGJmNzk1MjQ4MDlhODUxMWJmMzJkMDU3NTI2ZjdmMyJ9fX0");
     }
 }
