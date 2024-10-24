ef049ff874155e613444417560684ef6134a48a7
TeakIvy <legocjones@gmail.com>
2024-10-23 23:06:33 -0400

    Upgrade to 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ val libraries = listOf(
 )
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.3-R0.1-SNAPSHOT")
     compileOnly("com.mojang:authlib:1.5.25")
 
     libraries.forEach { library ->
