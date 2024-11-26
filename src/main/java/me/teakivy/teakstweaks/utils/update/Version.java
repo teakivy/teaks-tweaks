c014b8de406aaab9d27829568f39f774d91d5ed0
teakivy <legocjones@gmail.com>
2024-11-26 17:06:32 -0500

    Added Version & VersionManager utility classes

---
+++ src/main/java/me/teakivy/teakstweaks/utils/update/Version.java
@@ -0,0 +1,71 @@
+package me.teakivy.teakstweaks.utils.update;
+
+import org.bukkit.Bukkit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Version {
+    private final String version;
+    private final List<String> supportedMCVersions;
+    private final String id;
+
+    public Version(String version, List<String> supportedMCVersions, String id) {
+        this.version = version;
+        this.supportedMCVersions = supportedMCVersions;
+        this.id = id;
+    }
+
+    public Version(String version, String id) {
+        this.version = version;
+        this.supportedMCVersions = new ArrayList<>();
+        this.id = id;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public List<String> getSupportedMCVersions() {
+        return supportedMCVersions;
+    }
+
+    public String getUrl() {
+        return "https://modrinth.com/plugin/teaks-tweaks/version/" + id;
+    }
+
+    public boolean isSupported() {
+        String mcVersion = Bukkit.getBukkitVersion().split("-")[0];
+        return supportedMCVersions.contains(mcVersion);
+    }
+
+    public void addSupportedMCVersion(String version) {
+        supportedMCVersions.add(version);
+    }
+
+    public int getMajorVersion() {
+        return Integer.parseInt(version.split("\\.")[0]);
+    }
+
+    public int getMinorVersion() {
+        return Integer.parseInt(version.split("\\.")[1]);
+    }
+
+    public int getPatchVersion() {
+        return Integer.parseInt(version.split("\\.")[2]);
+    }
+
+    public boolean isNewerThan(Version v) {
+        if (getMajorVersion() != v.getMajorVersion()) {
+            return getMajorVersion() > v.getMajorVersion();
+        }
+        if (getMinorVersion() != v.getMinorVersion()) {
+            return getMinorVersion() > v.getMinorVersion();
+        }
+        if (getPatchVersion() != v.getPatchVersion()) {
+            return getPatchVersion() > v.getPatchVersion();
+        }
+        return (getSupportedMCVersions().size() < v.getSupportedMCVersions().size());
+    }
+
+}
