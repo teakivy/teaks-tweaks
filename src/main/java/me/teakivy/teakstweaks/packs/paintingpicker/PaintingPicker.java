e28efc913c3c08ee8ad80ae9ea5b94bdac733aa1
TeakIvy <legocjones@gmail.com>
2025-07-06 20:52:57 -0400

    Add Dennis Painting & Read paintings through registry

--- src/main/java/me/teakivy/teakstweaks/packs/paintingpicker/PaintingPicker.java
+++ src/main/java/me/teakivy/teakstweaks/packs/paintingpicker/PaintingPicker.java
@@ -1,17 +1,27 @@
 package me.teakivy.teakstweaks.packs.paintingpicker;
 
 import com.google.gson.internal.LinkedTreeMap;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.data.PaintingVariantRegistryEntry;
+import io.papermc.paper.registry.keys.PaintingVariantKeys;
+import io.papermc.paper.registry.keys.tags.PaintingVariantTagKeys;
 import me.teakivy.teakstweaks.TeaksTweaks;
 import me.teakivy.teakstweaks.packs.BasePack;
 import me.teakivy.teakstweaks.packs.PackType;
 import me.teakivy.teakstweaks.utils.ItemSerializer;
 import me.teakivy.teakstweaks.utils.JsonManager;
 import me.teakivy.teakstweaks.utils.Key;
+import org.bukkit.Art;
 import org.bukkit.Material;
+import org.bukkit.Registry;
+import org.bukkit.entity.Painting;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.StonecuttingRecipe;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -32,41 +42,8 @@ public class PaintingPicker extends BasePack {
         return data;
     }
 
-    static HashMap<String, ItemStack> paintings = new HashMap<>();
-
     public PaintingPicker() {
         super("painting-picker", PackType.TEAKSTWEAKS, Material.PAINTING);
-    }
-
-    public static void load() {
-        JsonManager.saveToFile(
-                JsonManager.updateJson(
-                        JsonManager.getFromFile("data/paintings.json"),
-                        JsonManager.getFromResource("data/paintings.json"),
-                        true),
-                "data/paintings.json");
-
-        File file = new File(TeaksTweaks.getInstance().getDataFolder() + "/data/paintings.json");
-        if (!file.exists()) TeaksTweaks.getInstance().saveResource("data/paintings.json", false);
-
-        file = new File(TeaksTweaks.getInstance().getDataFolder() + "/data/paintings.json");
-
-        if (!file.exists()) paintings = new HashMap<>();
-
-        try {
-            LinkedHashMap<String, Object> map =  TeaksTweaks.getGson().fromJson(new FileReader(file), LinkedHashMap.class);
-            for (Object painting : ((List<Object>) map.get("paintings"))) {
-                LinkedTreeMap<String, Object> paintings_map = (LinkedTreeMap<String, Object>) painting;
-
-                String name = (String) paintings_map.get("name");
-                String base64 = (String) paintings_map.get("base64");
-
-                paintings.put(name, ItemSerializer.fromByteArray(Base64.getDecoder().decode(base64)));
-            }
-            return;
-        } catch (FileNotFoundException ignored) {}
-
-        paintings = new HashMap<>();
     }
 
     @EventHandler
@@ -79,14 +56,17 @@ public class PaintingPicker extends BasePack {
     @Override
     public void init() {
         super.init();
-        load();
+
+        Registry<@NotNull Art> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.PAINTING_VARIANT);
 
         StonecuttingRecipe recipe = new StonecuttingRecipe(Key.get("painting_any"), new ItemStack(Material.PAINTING), Material.PAINTING);
         addRecipe(recipe);
 
-        for (Map.Entry<String, ItemStack> entry : paintings.entrySet()) {
-            String name = entry.getKey();
-            ItemStack item = entry.getValue();
+        for (Art art : registry) {
+            System.out.println(art);
+            String name = art.toString();
+            ItemStack item = ItemStack.of(Material.PAINTING);
+            item.setData(DataComponentTypes.PAINTING_VARIANT, art);
 
             recipe = new StonecuttingRecipe(Key.get("painting_" + name.toLowerCase().replaceAll(" ", "_")), item, Material.PAINTING);
             addRecipe(recipe);
