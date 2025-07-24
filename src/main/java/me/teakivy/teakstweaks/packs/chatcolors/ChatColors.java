1b6654426af0471acaaba013d268edd4dfefd33e
TeakIvy <legocjones@gmail.com>
2025-07-23 22:37:19 -0400

    Fixed broken chat messages with Chat Colors pack

--- src/main/java/me/teakivy/teakstweaks/packs/chatcolors/ChatColors.java
+++ src/main/java/me/teakivy/teakstweaks/packs/chatcolors/ChatColors.java
@@ -1,5 +1,6 @@
 package me.teakivy.teakstweaks.packs.chatcolors;
 
+import io.papermc.paper.event.player.AsyncChatEvent;
 import me.teakivy.teakstweaks.packs.BasePack;
 import me.teakivy.teakstweaks.utils.permission.Permission;
 import net.kyori.adventure.text.Component;
@@ -8,7 +9,6 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -19,15 +19,17 @@ public class ChatColors extends BasePack {
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
+    public void onChat(AsyncChatEvent event) {
         if (!Permission.CHAT_COLORS_CHAT.check(event.getPlayer())) return;
 
-        event.setMessage(legacyToMiniMessage(newText(event.getMessage())).toString());
+        MiniMessage mm = MiniMessage.miniMessage();
+        System.out.println(mm.serialize(legacyToMiniMessage(event.message())));
+        event.message(mm.deserialize(mm.serialize(legacyToMiniMessage(event.message())).replaceAll("\\\\<", "<")));
     }
 
     @EventHandler
     private void onAnvil(PrepareAnvilEvent event) {
-        if (!Permission.CHAT_COLORS_ANVIL.check(event.getInventory().getViewers().get(0))) return;
+        if (!Permission.CHAT_COLORS_ANVIL.check(event.getInventory().getViewers().getFirst())) return;
         ItemStack result = event.getResult();
         if (result == null) return;
         if (!result.hasItemMeta()) return;
@@ -36,7 +38,7 @@ public class ChatColors extends BasePack {
         if (meta == null) return;
         if (!meta.hasDisplayName()) return;
 
-        meta.displayName(legacyToMiniMessage(newText(meta.getDisplayName())));
+        meta.displayName(legacyToMiniMessage(meta.displayName()));
         result.setItemMeta(meta);
         event.setResult(result);
     }
@@ -47,35 +49,37 @@ public class ChatColors extends BasePack {
 
         for (int i = 0; i < event.lines().size(); i++) {
             if (event.line(i) == null) continue;
-            event.line(i, legacyToMiniMessage(newText(event.getLine(i))));
+            event.line(i, legacyToMiniMessage(event.line(i)));
         }
     }
 
     private Component legacyToMiniMessage(Component legacy) {
-        String text = MiniMessage.miniMessage().serialize(legacy);
-        text = text.replaceAll("&a", "<green>");
-        text = text.replaceAll("&b", "<aqua>");
-        text = text.replaceAll("&c", "<red>");
-        text = text.replaceAll("&d", "<light_purple>");
-        text = text.replaceAll("&e", "<yellow>");
-        text = text.replaceAll("&f", "<white>");
-        text = text.replaceAll("&0", "<black>");
-        text = text.replaceAll("&1", "<dark_blue>");
-        text = text.replaceAll("&2", "<dark_green>");
-        text = text.replaceAll("&3", "<dark_aqua>");
-        text = text.replaceAll("&4", "<dark_red>");
-        text = text.replaceAll("&5", "<dark_purple>");
-        text = text.replaceAll("&6", "<gold>");
-        text = text.replaceAll("&7", "<gray>");
-        text = text.replaceAll("&8", "<dark_gray>");
-        text = text.replaceAll("&9", "<blue>");
-        text = text.replaceAll("&k", "<obfuscated>");
-        text = text.replaceAll("&l", "<bold>");
-        text = text.replaceAll("&m", "<strikethrough>");
-        text = text.replaceAll("&n", "<underlined>");
-        text = text.replaceAll("&o", "<italic>");
-        text = text.replaceAll("&r", "<reset>");
-        return newText(text);
+        legacy = legacy.replaceText(builder -> builder.matchLiteral("&a").replacement("<green>"));
+//        String text = MiniMessage.miniMessage().serialize(legacy);
+//        text = text.replaceAll("&a", "<green>");
+//        text = text.replaceAll("&b", "<aqua>");
+//        text = text.replaceAll("&c", "<red>");
+//        text = text.replaceAll("&d", "<light_purple>");
+//        text = text.replaceAll("&e", "<yellow>");
+//        text = text.replaceAll("&f", "<white>");
+//        text = text.replaceAll("&0", "<black>");
+//        text = text.replaceAll("&1", "<dark_blue>");
+//        text = text.replaceAll("&2", "<dark_green>");
+//        text = text.replaceAll("&3", "<dark_aqua>");
+//        text = text.replaceAll("&4", "<dark_red>");
+//        text = text.replaceAll("&5", "<dark_purple>");
+//        text = text.replaceAll("&6", "<gold>");
+//        text = text.replaceAll("&7", "<gray>");
+//        text = text.replaceAll("&8", "<dark_gray>");
+//        text = text.replaceAll("&9", "<blue>");
+//        text = text.replaceAll("&k", "<obfuscated>");
+//        text = text.replaceAll("&l", "<bold>");
+//        text = text.replaceAll("&m", "<strikethrough>");
+//        text = text.replaceAll("&n", "<underlined>");
+//        text = text.replaceAll("&o", "<italic>");
+//        text = text.replaceAll("&r", "<reset>");
+//        return newText(text);
+        return legacy;
     }
 
 }
