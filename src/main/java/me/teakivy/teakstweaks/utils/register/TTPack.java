a52f42d7ed8ac8fa12bfcec6eb0c1721f98d0945
TeakIvy <legocjones@gmail.com>
2026-02-03 22:42:50 -0500

    Add easier-axolotl-breeding pack

--- src/main/java/me/teakivy/teakstweaks/utils/register/TTPack.java
+++ src/main/java/me/teakivy/teakstweaks/utils/register/TTPack.java
@@ -27,6 +27,7 @@ import me.teakivy.teakstweaks.packs.disposal.Disposal;
 import me.teakivy.teakstweaks.packs.doubleshulkershells.DoubleShulkerShells;
 import me.teakivy.teakstweaks.packs.dragondrops.DragonDrops;
 import me.teakivy.teakstweaks.packs.durabilityping.DurabilityPing;
+import me.teakivy.teakstweaks.packs.easieraxolotlbreeding.EasierAxolotlBreeding;
 import me.teakivy.teakstweaks.packs.elevators.Elevators;
 import me.teakivy.teakstweaks.packs.fastleafdecay.FastLeafDecay;
 import me.teakivy.teakstweaks.packs.fixeditemframes.FixedItemFrames;
@@ -108,6 +109,7 @@ public enum TTPack {
     DOUBLE_SHULKER_SHELLS("double-shulker-shells", DoubleShulkerShells.class),
     DRAGON_DROPS("dragon-drops", DragonDrops.class),
     DURABILITY_PING("durability-ping", DurabilityPing.class),
+    EASIER_AXOLOTL_BREEDING("easier-axolotl-breeding", EasierAxolotlBreeding.class),
     ELEVATORS("elevators", Elevators.class),
     FAST_LEAF_DECAY("fast-leaf-decay", FastLeafDecay.class),
     FIXED_ITEM_FRAMES("fixed-item-frames", FixedItemFrames.class),
