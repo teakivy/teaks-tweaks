df86e997e18d3eac0f5c73dcc7f67536b54c4f45
TeakIvy <legocjones@gmail.com>
2026-06-14 20:24:03 -0400

    Add hand swing animation & actionbar feedback for Easier Mending pack

--- src/main/java/me/teakivy/teakstweaks/packs/easiermending/EasierMending.java
+++ src/main/java/me/teakivy/teakstweaks/packs/easiermending/EasierMending.java
@@ -1,13 +1,17 @@
 package me.teakivy.teakstweaks.packs.easiermending;
 
 import me.teakivy.teakstweaks.packs.BasePack;
+import me.teakivy.teakstweaks.utils.StringUtils;
 import me.teakivy.teakstweaks.utils.register.TTPack;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -37,18 +41,33 @@ public class EasierMending extends BasePack {
         int durability = meta.getDamage();
         if (durability <= 0) return;
         int xp = player.calculateTotalExperiencePoints();
+        player.swingHand(event.getHand() == EquipmentSlot.OFF_HAND ? EquipmentSlot.OFF_HAND : EquipmentSlot.HAND);
+
+        Component itemName = null;
+        if (item.hasItemMeta() && item.getItemMeta().hasDisplayName()) {
+            itemName = item.getItemMeta().displayName();
+        }
+        if (itemName == null) {
+            itemName = Component.text(StringUtils.toTitleCase(item.getType().name().replace("_", " ")));
+        }
+
+
+
         if (xp > durability / 2) {
             meta.setDamage(0);
             item.setItemMeta(meta);
             player.setExperienceLevelAndProgress(xp - durability / 2);
 
             event.setCancelled(true);
+            player.sendActionBar(Component.text("Repaired ").append(itemName).append(Component.text(" for " + (durability / 2) + " XP!")).color(TextColor.color(0x00FF00)));
             return;
         }
 
         meta.setDamage(durability - xp * 2);
         item.setItemMeta(meta);
         player.setExperienceLevelAndProgress(0);
+
+        player.sendActionBar(Component.text("Repaired ").append(itemName).append(Component.text(" for " + xp + " XP!")).color(TextColor.color(0x00FF00)));
         event.setCancelled(true);
     }
 
