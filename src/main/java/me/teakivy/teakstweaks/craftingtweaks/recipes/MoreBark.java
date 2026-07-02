6cdb7af2f4ca58161a1d907926b0e281620d036b
TeakIvy <legocjones@gmail.com>
2026-07-02 18:53:52 -0400

    Reworked More Bark to be generic

--- src/main/java/me/teakivy/teakstweaks/craftingtweaks/recipes/MoreBark.java
+++ src/main/java/me/teakivy/teakstweaks/craftingtweaks/recipes/MoreBark.java
@@ -6,6 +6,7 @@ import me.teakivy.teakstweaks.utils.register.TTCraftingTweak;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Tag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
@@ -17,28 +18,15 @@ public class MoreBark extends AbstractCraftingTweak {
 
     @Override
     public void registerRecipes() {
-        newBarkedRecipe(Material.OAK_LOG, Material.OAK_WOOD);
-        newBarkedRecipe(Material.STRIPPED_OAK_LOG, Material.STRIPPED_OAK_WOOD);
-        newBarkedRecipe(Material.DARK_OAK_LOG, Material.DARK_OAK_WOOD);
-        newBarkedRecipe(Material.STRIPPED_DARK_OAK_LOG, Material.STRIPPED_DARK_OAK_WOOD);
-        newBarkedRecipe(Material.BIRCH_LOG, Material.BIRCH_WOOD);
-        newBarkedRecipe(Material.STRIPPED_BIRCH_LOG, Material.STRIPPED_BIRCH_WOOD);
-        newBarkedRecipe(Material.SPRUCE_LOG, Material.SPRUCE_WOOD);
-        newBarkedRecipe(Material.STRIPPED_SPRUCE_LOG, Material.STRIPPED_SPRUCE_WOOD);
-        newBarkedRecipe(Material.ACACIA_LOG, Material.ACACIA_WOOD);
-        newBarkedRecipe(Material.STRIPPED_ACACIA_LOG, Material.STRIPPED_ACACIA_WOOD);
-        newBarkedRecipe(Material.JUNGLE_LOG, Material.JUNGLE_WOOD);
-        newBarkedRecipe(Material.STRIPPED_JUNGLE_LOG, Material.STRIPPED_JUNGLE_WOOD);
-        newBarkedRecipe(Material.CRIMSON_STEM, Material.CRIMSON_HYPHAE);
-        newBarkedRecipe(Material.STRIPPED_CRIMSON_STEM, Material.STRIPPED_CRIMSON_HYPHAE);
-        newBarkedRecipe(Material.WARPED_STEM, Material.WARPED_HYPHAE);
-        newBarkedRecipe(Material.STRIPPED_WARPED_STEM, Material.STRIPPED_WARPED_HYPHAE);
-        newBarkedRecipe(Material.MANGROVE_LOG, Material.MANGROVE_WOOD);
-        newBarkedRecipe(Material.STRIPPED_MANGROVE_LOG, Material.STRIPPED_MANGROVE_WOOD);
-        newBarkedRecipe(Material.CHERRY_LOG, Material.CHERRY_WOOD);
-        newBarkedRecipe(Material.STRIPPED_CHERRY_LOG, Material.STRIPPED_CHERRY_WOOD);
-        newBarkedRecipe(Material.PALE_OAK_LOG, Material.PALE_OAK_WOOD);
-        newBarkedRecipe(Material.STRIPPED_PALE_OAK_LOG, Material.STRIPPED_PALE_OAK_WOOD);
+        Tag.LOGS.getValues().forEach(log -> {
+            if (log.name().endsWith("_WOOD") || log.name().endsWith("_HYPHAE")) return;
+
+            Material wood = Material.getMaterial(log.name().replace("_LOG", "_WOOD"));
+            if (wood == null) wood = Material.getMaterial(log.name().replace("_LOG", "_HYPHAE"));
+            if (wood == null) return;
+
+            newBarkedRecipe(log, wood);
+        });
     }
 
     public void newBarkedRecipe(Material input, Material output) {
