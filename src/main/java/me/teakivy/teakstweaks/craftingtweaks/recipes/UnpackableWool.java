6579ce9a848521b3085675be37ddf4cb7d5b5ac9
TeakIvy <legocjones@gmail.com>
2026-07-02 19:02:10 -0400

    Reworked Unpackable Wool to use Tags

--- src/main/java/me/teakivy/teakstweaks/craftingtweaks/recipes/UnpackableWool.java
+++ src/main/java/me/teakivy/teakstweaks/craftingtweaks/recipes/UnpackableWool.java
@@ -5,6 +5,7 @@ import me.teakivy.teakstweaks.utils.Key;
 import me.teakivy.teakstweaks.utils.register.TTCraftingTweak;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapelessRecipe;
@@ -20,24 +21,7 @@ public class UnpackableWool extends AbstractCraftingTweak {
 
     @Override
     public void registerRecipes() {
-        List<Material> woolList = new ArrayList<>();
-        woolList.add(Material.WHITE_WOOL);
-        woolList.add(Material.ORANGE_WOOL);
-        woolList.add(Material.MAGENTA_WOOL);
-        woolList.add(Material.LIGHT_BLUE_WOOL);
-        woolList.add(Material.YELLOW_WOOL);
-        woolList.add(Material.LIME_WOOL);
-        woolList.add(Material.PINK_WOOL);
-        woolList.add(Material.GRAY_WOOL);
-        woolList.add(Material.LIGHT_GRAY_WOOL);
-        woolList.add(Material.CYAN_WOOL);
-        woolList.add(Material.PURPLE_WOOL);
-        woolList.add(Material.BLUE_WOOL);
-        woolList.add(Material.BROWN_WOOL);
-        woolList.add(Material.GREEN_WOOL);
-        woolList.add(Material.RED_WOOL);
-        woolList.add(Material.BLACK_WOOL);
-        RecipeChoice wool = new RecipeChoice.MaterialChoice(woolList);
+        RecipeChoice wool = new RecipeChoice.MaterialChoice(Tag.WOOL);
 
         ShapelessRecipe recipe = new ShapelessRecipe(Key.get("wool_unpackables"),
                 new ItemStack(Material.STRING, 4));
