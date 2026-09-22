d2c77b13c3c2d370663b7f8031ea8d43545eedf8
TeakIvy <legocjones@gmail.com>
2026-09-22 12:17:05 -0400

    Rework ChatColors

--- src/main/java/me/teakivy/teakstweaks/packs/chatcolors/ChatColors.java
+++ src/main/java/me/teakivy/teakstweaks/packs/chatcolors/ChatColors.java
@@ -2,6 +2,7 @@ package me.teakivy.teakstweaks.packs.chatcolors;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.teakivy.teakstweaks.packs.BasePack;
+import me.teakivy.teakstweaks.utils.StringUtils;
 import me.teakivy.teakstweaks.utils.permission.Permission;
 import me.teakivy.teakstweaks.utils.register.TTPack;
 import net.kyori.adventure.text.Component;
@@ -24,7 +25,7 @@ public class ChatColors extends BasePack {
         if (!Permission.CHAT_COLORS_CHAT.check(event.getPlayer())) return;
 
         MiniMessage mm = MiniMessage.miniMessage();
-        event.message(mm.deserialize(mm.serialize(legacyToMiniMessage(event.message())).replaceAll("\\\\<", "<")));
+        event.message(mm.deserialize(mm.serialize(StringUtils.parseLegacyChatColors(event.message())).replaceAll("\\\\<", "<")));
     }
 
     @EventHandler
@@ -39,7 +40,7 @@ public class ChatColors extends BasePack {
         if (!meta.hasDisplayName()) return;
 
         MiniMessage mm = MiniMessage.miniMessage();
-        meta.displayName(mm.deserialize(mm.serialize(legacyToMiniMessage(meta.displayName())).replaceAll("\\\\<", "<")));
+        meta.displayName(mm.deserialize(mm.serialize(StringUtils.parseLegacyChatColors(meta.displayName())).replaceAll("\\\\<", "<")));
         result.setItemMeta(meta);
         event.setResult(result);
     }
@@ -51,36 +52,8 @@ public class ChatColors extends BasePack {
 
         for (int i = 0; i < event.lines().size(); i++) {
             if (event.line(i) == null) continue;
-            event.line(i, mm.deserialize(mm.serialize(legacyToMiniMessage(event.line(i))).replaceAll("\\\\<", "<")));
+            event.line(i, mm.deserialize(mm.serialize(StringUtils.parseLegacyChatColors(event.line(i))).replaceAll("\\\\<", "<")));
         }
-    }
-
-    private Component legacyToMiniMessage(Component legacy) {
-        legacy = legacy.replaceText(b -> b.matchLiteral("&a").replacement("<green>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&b").replacement("<aqua>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&c").replacement("<red>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&d").replacement("<light_purple>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&e").replacement("<yellow>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&f").replacement("<white>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&0").replacement("<black>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&1").replacement("<dark_blue>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&2").replacement("<dark_green>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&3").replacement("<dark_aqua>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&4").replacement("<dark_red>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&5").replacement("<dark_purple>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&6").replacement("<gold>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&7").replacement("<gray>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&8").replacement("<dark_gray>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&9").replacement("<blue>"));
-
-        legacy = legacy.replaceText(b -> b.matchLiteral("&k").replacement("<obfuscated>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&l").replacement("<bold>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&m").replacement("<strikethrough>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&n").replacement("<underlined>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&o").replacement("<italic>"));
-        legacy = legacy.replaceText(b -> b.matchLiteral("&r").replacement("<reset>"));
-
-        return legacy;
     }
 
 }
--- src/main/java/me/teakivy/teakstweaks/utils/StringUtils.java
+++ src/main/java/me/teakivy/teakstweaks/utils/StringUtils.java
@@ -1,5 +1,9 @@
 package me.teakivy.teakstweaks.utils;
 
+import net.kyori.adventure.text.Component;
+
+import java.util.HashMap;
+
 public final class StringUtils {
 
     public static String toTitleCase(String input) {
@@ -29,5 +33,40 @@ public final class StringUtils {
         }
 
         return result.toString();
+    }
+
+    public static Component parseLegacyChatColors(Component legacy) {
+        HashMap<Character, String> colorMap = new HashMap<>();
+        // colors
+        colorMap.put('a', "green");
+        colorMap.put('b', "aqua");
+        colorMap.put('c', "red");
+        colorMap.put('d', "light_purple");
+        colorMap.put('e', "yellow");
+        colorMap.put('f', "white");
+        colorMap.put('0', "black");
+        colorMap.put('1', "dark_blue");
+        colorMap.put('2', "dark_green");
+        colorMap.put('3', "dark_aqua");
+        colorMap.put('4', "dark_red");
+        colorMap.put('5', "dark_purple");
+        colorMap.put('6', "gold");
+        colorMap.put('7', "gray");
+        colorMap.put('8', "dark_gray");
+        colorMap.put('9', "blue");
+        // effects
+        colorMap.put('k', "obfuscated");
+        colorMap.put('l', "bold");
+        colorMap.put('m', "strikethrough");
+        colorMap.put('n', "underlined");
+        colorMap.put('o', "italic");
+        colorMap.put('r', "reset");
+
+        for (char key : colorMap.keySet()) {
+            String replacement = "<" + colorMap.get(key) + ">";
+            legacy = legacy.replaceText(b -> b.matchLiteral("&" + key).replacement(replacement));
+        }
+
+        return legacy;
     }
 }
