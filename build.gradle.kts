9ffdc53a5c5a658118fcca4de7d0411e1f4c5eba
teakivy <legocjones@gmail.com>
2026-09-25 22:34:01 -0400

    Update to v2.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "me.teakivy"
-version = "2.1.4"
+version = "2.1.5"
 description = "150+ Toggleable Tweaks & Features including Vanilla Tweaks as a plugin, and more!"
 java.sourceCompatibility = JavaVersion.VERSION_25
 
